f77b8d546e6513445d4292f3736d425a534b7846
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-03-01 04:26:15 -0300

    Added weapon swaying movement

--- Assets/Scripts/Entities/EnemySpinner/CrabEnemy.cs
+++ Assets/Scripts/Entities/EnemySpinner/CrabEnemy.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class CrabEnemy : MonoBehaviour
 {
     [SerializeField] private float timeToMove;
+    [SerializeField] private float timeToRotateWeapons;
     [SerializeField] AnimationCurve speedCurve;
-    [SerializeField] private float speed;
+    [SerializeField] AnimationCurve rotateSpeedCurve;
+    [SerializeField] private float moveSpeedMultiplier;
+    [SerializeField] private float rotateSpeedMultiplier;
+    [SerializeField] private Transform[] weaponsTransforms;
 
     private Rigidbody rb;
     private HealthController healthController;
+
     private bool movingRight = true;
     private float currentMoveTime = 0;
+
+    private bool rotatingUp = true;
+    private float currentWeaponRotateTime = 0;
 
     private void Start()
     {
         if (!TryGetComponent(out healthController) || !TryGetComponent(out rb))
         {
-            Debug.LogError(gameObject.name + " needs a " + nameof(HealthController) + " and a Rigidbody " + " to work properly!");
+            Debug.LogError(gameObject.name + " needs a " + nameof(HealthController) + " and a Rigidbody to work properly!");
             gameObject.SetActive(false);
         }
     }
@@ -32,13 +40,14 @@ public class CrabEnemy : MonoBehaviour
     {
         float normTime = currentMoveTime / timeToMove;
         float moveSpeed = speedCurve.Evaluate(normTime);
+
         if (movingRight)
         {
-            transform.Translate(transform.right * moveSpeed * speed * Time.deltaTime, Space.Self);
+            transform.Translate(transform.right * moveSpeed * moveSpeedMultiplier * Time.deltaTime, Space.Self);
         }
         else
         {
-            transform.Translate(-transform.right * moveSpeed * speed * Time.deltaTime, Space.Self);
+            transform.Translate(-transform.right * moveSpeed * moveSpeedMultiplier * Time.deltaTime, Space.Self);
         }
 
         currentMoveTime += Time.deltaTime;
@@ -52,6 +61,35 @@ public class CrabEnemy : MonoBehaviour
 
     private void WeaponMovement()
     {
+        float normTime = currentWeaponRotateTime / timeToRotateWeapons;
+        float rotateSpeed = rotateSpeedCurve.Evaluate(normTime);
 
+        for (int i = 0; i < weaponsTransforms.Length; i++)
+        {
+            Vector3 newRotation = Vector3.zero;
+
+            if (rotatingUp)
+            {
+                newRotation -= new Vector3(rotateSpeed,0, 0 ) * rotateSpeedMultiplier * Time.deltaTime;
+                //transform.localEulerAngles = newRotation;
+                weaponsTransforms[i].Rotate(newRotation, Space.Self);
+            }
+            else
+            {
+                newRotation += new Vector3(rotateSpeed, 0, 0) * rotateSpeedMultiplier * Time.deltaTime;
+                //transform.localEulerAngles = newRotation;
+                weaponsTransforms[i].Rotate(newRotation, Space.Self);
+            }
+        }
+
+
+
+        currentWeaponRotateTime += Time.deltaTime;
+
+        if (currentWeaponRotateTime > timeToRotateWeapons)
+        {
+            rotatingUp = !rotatingUp;
+            currentWeaponRotateTime = 0;
+        }
     }
 }
