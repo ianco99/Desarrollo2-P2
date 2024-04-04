1e51d3fbab2c3f2a742da00fc6b13154f53fc1c1
Ian Kuznicki <iankuz25@gmail.com>
2024-04-04 19:57:54 -0300

    Created player character events

---
+++ Assets/Scripts/EventChannels/PlayerCharacterEventChannel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kuznickiEventChannel
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/EventChannelSOs/PlayerCharacterEventChannel", fileName = "PlayerCharacterEventChannel")]
+    public class PlayerCharacterEventChannel : EventChannelSO<PlayerCharacter>
+    {
+        
+    }
+}
